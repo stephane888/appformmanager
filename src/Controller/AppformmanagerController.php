8ef7fd3fbc14055bfb519823792586b6ab8a034d
stephane888 <kksasteph888@gmail.com>
2021-06-08 07:21:01 +0100

    MAJ

--- src/Controller/AppformmanagerController.php
+++ src/Controller/AppformmanagerController.php
@@ -8,18 +8,20 @@ use Drupal\Core\Controller\ControllerBase;
  * Returns responses for AppFormManager routes.
  */
 class AppformmanagerController extends ControllerBase {
-
-  /**
-   * Builds the response.
-   */
-  public function build() {
-
-    $build['content'] = [
-      '#type' => 'item',
-      '#markup' => $this->t('It works!'),
-    ];
-
-    return $build;
-  }
-
+	
+	/**
+	 * Builds the response.
+	 */
+	public function build(){
+		$configs = [
+				'#type'=> "html_tag",
+				'#tag'=> 'section',
+				'#value'=> 'Travaux de renovations',
+				'#attributes'=> [
+						'id'=> 'app'
+				]
+		];
+		$configs['#attached']['library'][] = 'appformmanager/app_form';
+		return $configs;
+	}
 }
