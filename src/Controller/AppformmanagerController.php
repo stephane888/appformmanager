bfdea69934bbf208034d182c02dd7adcd4198001
stephane888 <kksasteph888@gmail.com>
2021-06-30 02:45:44 +0100

    MAJ

--- src/Controller/AppformmanagerController.php
+++ src/Controller/AppformmanagerController.php
@@ -3,6 +3,8 @@
 namespace Drupal\appformmanager\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Drupal\Component\Serialization\Json;
 
 /**
  * Returns responses for AppFormManager routes.
@@ -39,6 +41,45 @@ class AppformmanagerController extends ControllerBase {
         ];
         $configs['#attached']['library'][] = 'appformmanager/manager';
         return $configs;
+    }
+
+    public function user() {
+        $id = \Drupal::currentUser()->id();
+        if ($id) {
+            $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+            $serializer = \Drupal::service('serializer');
+            $data = $serializer->serialize($user, 'json', [
+              'plugin_id' => 'entity'
+                ]);
+        }
+        else {
+            $data = $id;
+        }
+        return $this->reponse($data);
+    }
+
+    /**
+     * Enregistrer un utilisateur.
+     */
+    public function register() {
+        
+    }
+
+    /**
+     *
+     * @param array|string $configs
+     * @param number $code
+     * @param string $message
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    protected function reponse($configs, $code = null, $message = null) {
+        if (!is_string($configs))
+            $configs = Json::encode($configs);
+        $reponse = new JsonResponse();
+        if ($code)
+            $reponse->setStatusCode($code, $message);
+        $reponse->setContent($configs);
+        return $reponse;
     }
 
 }
---
+++ src/EventSubscriber/AppFormmanagerListenerEvent.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\appformmanager\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\serialization\EventSubscriber\DefaultExceptionSubscriber;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Drupal\Core\Cache\CacheableDependencyInterface;
+use Drupal\Core\Cache\CacheableResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Class EntityTypeSubscriber.
+ *
+ * @package Drupal\custom_events\EventSubscriber
+ */
+class AppFormmanagerListenerEvent extends DefaultExceptionSubscriber {
+	use StringTranslationTrait;
+	/**
+	 * DefaultExceptionSubscriber constructor.
+	 *
+	 * @param \Symfony\Component\Serializer\SerializerInterface $serializer
+	 *        	The serializer service.
+	 * @param array $serializer_formats
+	 *        	The available serialization formats.
+	 */
+	public function __construct(SerializerInterface $serializer, array $serializer_formats){
+		$this->serializer = $serializer;
+		$this->serializerFormats = $serializer_formats;
+	}
+	
+	/**
+	 * Handles all 4xx errors for all serialization failures.
+	 *
+	 * @param \Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent $event
+	 *        	The event to process.
+	 */
+	public function on4xx(GetResponseForExceptionEvent $event){
+		/** @var \Symfony\Component\HttpKernel\Exception\HttpExceptionInterface $exception */
+		// $event->ex
+		/**
+		 *
+		 * @var \Exception $exception
+		 */
+		$exception = $event->getException();
+		$request = $event->getRequest();
+		
+		$format = $request->getRequestFormat();
+		// messages
+		$errorsMsg = explode( "\n", $exception->getMessage() );
+		if(! empty( $errorsMsg ) && \strstr( $errorsMsg[0], "Unprocessable Entity" ) !== false){
+			$errorsMsg[0] = $this->t( $errorsMsg[0] );
+		}
+		$content = [
+				'message'=> $exception->getMessage(),
+				'errors'=> $errorsMsg
+			// 'file'=> $exception->getTrace()
+		];
+		$encoded_content = $this->serializer->serialize( $content, $format );
+		$headers = $exception->getHeaders();
+		
+		// Add the MIME type from the request to send back in the header.
+		$headers['Content-Type'] = $request->getMimeType( $format );
+		
+		// If the exception is cacheable, generate a cacheable response.
+		if($exception instanceof CacheableDependencyInterface){
+			$response = new CacheableResponse( $encoded_content, $exception->getStatusCode(), $headers );
+			$response->addCacheableDependency( $exception );
+		}
+		else{
+			$response = new Response( $encoded_content, $exception->getStatusCode(), $headers );
+		}
+		
+		$event->setResponse( $response );
+	}
+}
