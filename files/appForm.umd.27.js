25b3dc92494edb8d3d1b994725f2fc4777486652
kouwa stephane <kksasteph888@gmail.com>
2023-12-10 01:18:08 +0100

    Maj

---
+++ files/appForm.umd.27.js
@@ -0,0 +1,140 @@
+((typeof self !== 'undefined' ? self : this)["webpackJsonpappForm"] = (typeof self !== 'undefined' ? self : this)["webpackJsonpappForm"] || []).push([[27],{
+
+/***/ "c40c":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"09ade5b7-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/App/EditsFields/inputRecap.vue?vue&type=template&id=4efb913d&scoped=true&lang=html&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_c('b-row',{staticClass:"mb-3"},[_c('b-col',{attrs:{"sm":"12"}},[_c('compositeHeaderField',{attrs:{"field":_vm.field}})],1),_c('b-col',{attrs:{"sm":"12"}},[_c('b-form-group',[_c('ckeditor',{attrs:{"editor":_vm.editor,"config":_vm.editorConfig},model:{value:(_vm.field.value),callback:function ($$v) {_vm.$set(_vm.field, "value", $$v)},expression:"field.value"}})],1)],1),_c('b-col',{attrs:{"sm":"12"}},[_c('b-form-group',{attrs:{"label":"Coeficient pour estimation de prix en %"}},[_c('b-form-input',{attrs:{"required":"","type":"number"},model:{value:(_vm.field.percent),callback:function ($$v) {_vm.$set(_vm.field, "percent", $$v)},expression:"field.percent"}})],1),_c('b-form-group',{attrs:{"label":"Logique de calcul complexe"}},[_c('b-form-checkbox',{attrs:{"size":"lg"},model:{value:(_vm.field.complex_logique),callback:function ($$v) {_vm.$set(_vm.field, "complex_logique", $$v)},expression:"field.complex_logique"}})],1),(_vm.field.complex_logique)?_c('b-form-group',{attrs:{"label":"Logique calcul complexe"}},[_c('b-form-textarea',{attrs:{"placeholder":"","rows":"10"},model:{value:(_vm.field.preproccess_value),callback:function ($$v) {_vm.$set(_vm.field, "preproccess_value", $$v)},expression:"field.preproccess_value"}})],1):_vm._e()],1)],1)],1)}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./src/App/EditsFields/inputRecap.vue?vue&type=template&id=4efb913d&scoped=true&lang=html&
+
+// EXTERNAL MODULE: ./node_modules/@ckeditor/ckeditor5-vue2/dist/ckeditor.js
+var ckeditor = __webpack_require__("6930");
+var ckeditor_default = /*#__PURE__*/__webpack_require__.n(ckeditor);
+
+// EXTERNAL MODULE: ./src/App/EditsFields/compositeHeaderField.vue + 4 modules
+var compositeHeaderField = __webpack_require__("d3ee");
+
+// EXTERNAL MODULE: ./node_modules/@ckeditor/ckeditor5-build-classic/build/ckeditor.js
+var build_ckeditor = __webpack_require__("fb3d");
+var build_ckeditor_default = /*#__PURE__*/__webpack_require__.n(build_ckeditor);
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/App/EditsFields/inputRecap.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+
+/* harmony default export */ var inputRecapvue_type_script_lang_js_ = ({
+  name: "InputText",
+  props: {
+    field: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    compositeHeaderField: compositeHeaderField["default"],
+    ckeditor: ckeditor_default.a.component
+  },
+  data: function data() {
+    return {
+      editor: build_ckeditor_default.a,
+      editorData: "",
+      editorConfig: {// The configuration of the editor.
+      }
+    };
+  },
+  mounted: function mounted() {
+    if (this.field.percent === undefined) {
+      this.$set(this.field, "percent", 20);
+    }
+  },
+  watch: {//
+  },
+  computed: {//
+  },
+  methods: {//,
+  }
+});
+// CONCATENATED MODULE: ./src/App/EditsFields/inputRecap.vue?vue&type=script&lang=js&
+ /* harmony default export */ var EditsFields_inputRecapvue_type_script_lang_js_ = (inputRecapvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__("2877");
+
+// CONCATENATED MODULE: ./src/App/EditsFields/inputRecap.vue
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  EditsFields_inputRecapvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  "4efb913d",
+  null
+  
+)
+
+/* harmony default export */ var inputRecap = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=appForm.umd.27.js.map
